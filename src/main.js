478b6f1a5027608fe91d5ba5432f2e24a08abdc5
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-21 12:47:33 -0700

    WIP: vowelNumber prototype
    Co-authored-by: Brandon Eads <brandon181989@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -14,20 +14,27 @@ function Entry(title, body) {
 
 let entry1 = new Entry('My first post', 'Hey everybody this is my new blog')
 
+let vowelNumber = 
+
 Entry.prototype.numberOfWords = function() {
   let wordCount = 0;
   let text = this.Body;
-
   const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
     }
   });
-  return wordCount
+  return wordCount;
   } 
-  entry1.numberOfWords()
 
+
+Entry.prototype.numberOfVowels = function() {
+  let 
+
+}
+
+/[aieou]/gi
 
 //User Interface Logic
 
