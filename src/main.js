b7545b37ed65480932e9da9897d3b6c6108df537
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-21 11:00:14 -0700

    add Entry object and word counter prototype
    Co-authored-by: Brandon Eads <brandon181989@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
-import $ from 'jquery';
 import '../css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import $ from 'jquery';
+
+// Business Logic
+
+// New Entry object 
+
+function Entry(title, body) {
+  this.Title = title
+  this.Body = body
+}
+
+let entry1 = new Entry('My first post', 'Hey everybody this is my new blog')
+
+Entry.prototype.numberOfWords = function() {
+  let wordCount = 0;
+  let text = this.Body;
+
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount
+  } 
+  entry1.numberOfWords()
+
+
+//User Interface Logic
+
+let text = $("#wordcounter").val();
